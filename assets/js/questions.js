101dddd1c9e5bf5b87fd50e1f74d772a962485a5
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-01-20 18:20:03 +0000

    Created 3 JS files

---
+++ assets/js/questions.js
@@ -0,0 +1,16 @@
+Questions = [
+    {
+        question: "What is Javascript primarily used for in web development?",
+        answer1: "Defining the layout of the page",
+        answer2: "Styling the page",
+        answer3: "Adding interactivity to the page",
+        answer4: "Storing information"
+    },
+    {
+        question: "What is Javascript primarily used for in web development?",
+        answer1: "Defining the layout of the page",
+        answer2: "Styling the page",
+        answer3: "Adding interactivity to the page",
+        answer4: "Storing information"
+    }
+]
