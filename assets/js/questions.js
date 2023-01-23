6af08de44b07f46210e4be954c47a41d4aa51a19
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-01-23 23:20:17 +0000

    Changed list element attribute

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,13 +4,15 @@ let Questions = [
         answer1: "Defining the layout of the page",
         answer2: "Styling the page",
         answer3: "Adding interactivity to the page",
-        answer4: "Storing information"
+        answer4: "Storing information",
     },
     {
         question: "What is Javascript question 2?",
         answer1: "Answer 11111",
         answer2: "Answer 2222222",
         answer3: "Answer 3333333333333333",
-        answer4: "Answer 4444444444444444444444444"
+        answer4: "Answer 4444444444444444444444444",
     }
 ]
+
+console.log((Questions[0].answer3))
